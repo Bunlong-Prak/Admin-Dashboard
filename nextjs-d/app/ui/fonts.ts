cb5b13c9548bed9cbfcb193c64a29ab12b0b0335
Bunlong-Prak <100819214+Bunlong-Prak@users.noreply.github.com>
2024-10-14 18:45:51 +0700

    frist push

---
+++ nextjs-d/app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Lusitana, Edu_NSW_ACT_Foundation } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const lusitana = Lusitana({
+    weight: ["400", "700"],
+    subsets: ["latin"],
+});
+export const edu = Edu_NSW_ACT_Foundation({
+    weight: ["400", "700"],
+    subsets: ["latin"],
+});
